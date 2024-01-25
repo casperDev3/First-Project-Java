ee491a54d02bb8aa916396a1b6235186daaf982d
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-25 10:18:50 +0200

    addd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //! Reference Types / Objects
+        int age = 12;
+        Point pointA = new Point(10, 10); 
+    }
+
+    private void primitives(){
+        //! primitives
+        int numOne = 1;
+        double numTwo = 3.3;
+        double result = numOne + numTwo;
+
+        boolean isAdult = true;
+        char a = 'a';
+        byte b = 127;
+        short shNum = 32000;
+
+    }
+    private void strings(){
+        String text = "hello!";
+        System.out.println(text.toUpperCase());
+        System.out.println(text.isBlank());
+        System.out.println(text.length());
     }
 }
